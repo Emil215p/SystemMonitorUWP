4125d97fd5a8653e51bd4f3a9efce0f4d7fac5b4
Emil Korsholm <juelemil@gmail.com>
2025-11-15 13:32:38 +0100

    Disabled .NET Native compilation for Release due to issues with OpenHardwareMonitorLib.

--- SystemMonitorUWP/Pages/InfoPage.xaml.cs
+++ SystemMonitorUWP/Pages/InfoPage.xaml.cs
@@ -13,7 +13,6 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Microsoft.Toolkit.Uwp.Helpers;
-using OpenHardwareMonitor.Hardware.Gpu;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
