d392aeab4551034ea15723cef4e266bba01e0d8a
Emil Korsholm <juelemil@gmail.com>
2025-11-18 11:08:53 +0100

    Ability to get processor info.

--- SystemMonitorUWP/Pages/InfoPage.xaml.cs
+++ SystemMonitorUWP/Pages/InfoPage.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Navigation;
 using Microsoft.Toolkit.Uwp.Helpers;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -29,6 +30,7 @@ namespace SystemMonitorUWP.Pages
         public string OSVersion { get; set; }
         public string NETVersion { get; set; }
         public string OSArch { get; set; }
+        public string SysArch { get; set; }
 
         public InfoPage()
         {
@@ -36,7 +38,6 @@ namespace SystemMonitorUWP.Pages
 
             try
             {
-                Debug.WriteLine(RuntimeInformation.ProcessArchitecture);
                 LoadInfo();
             }
             catch (Exception ex)
@@ -52,7 +53,42 @@ namespace SystemMonitorUWP.Pages
             this.OSName = "OS Name: " + RuntimeInformation.OSDescription;
             this.OSVersion = "OS Version: " + SystemInformation.Instance.OperatingSystemVersion.ToString();
             this.OSArch = "OS Architecture: " + RuntimeInformation.OSArchitecture;
+            this.SysArch = "Processor Architecture: " + GetProcessorArchitecture();
             this.NETVersion = ".NET Version: " + RuntimeInformation.FrameworkDescription;
         }
+
+        private string GetProcessorArchitecture()
+        {
+            SYSTEM_INFO systemInfo = new SYSTEM_INFO();
+            GetNativeSystemInfo(ref systemInfo);
+
+            switch (systemInfo.wProcessorArchitecture)
+            {
+                case 0: return "x86";
+                case 9: return "x64";
+                case 5: return "ARM";
+                case 12: return "ARM64";
+                default: return "Unknown";
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct SYSTEM_INFO
+        {
+            public ushort wProcessorArchitecture;
+            public ushort wReserved;
+            public uint dwPageSize;
+            public IntPtr lpMinimumApplicationAddress;
+            public IntPtr lpMaximumApplicationAddress;
+            public IntPtr dwActiveProcessorMask;
+            public uint dwNumberOfProcessors;
+            public uint dwProcessorType;
+            public uint dwAllocationGranularity;
+            public ushort wProcessorLevel;
+            public ushort wProcessorRevision;
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern void GetNativeSystemInfo(ref SYSTEM_INFO lpSystemInfo);
     }
 }
