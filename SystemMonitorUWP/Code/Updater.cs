4a2e05f43431a2451c5e7c4e4cd4ad31043d95d2
Emil Korsholm <juelemil@gmail.com>
2026-01-05 14:19:05 +0100

    Added updater code page, albeit very early. Current version now shows in About.

---
+++ SystemMonitorUWP/Code/Updater.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Toolkit.Uwp.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using System.Diagnostics;
+
+namespace SystemMonitorUWP.Code
+{
+    public class Updater
+    {
+        private static readonly Updater _instance = new Updater();
+        public static Updater Instance => _instance;
+
+        public bool updateAvailable = false;
+        public bool isCheckingForUpdate = false;
+        public string latestVersion = "0.0.0";
+        public string downloadLink = "";
+        public string releaseNotes = "";
+        public string releaseDate = "";
+        public string currentVersion = PackageVersionHelper.ToFormattedString(Package.Current.Id.Version);
+
+        public void Check_Update()
+        {
+
+        }
+
+    }
+}
--- SystemMonitorUWP/Pages/AboutPage.xaml.cs
+++ SystemMonitorUWP/Pages/AboutPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using SystemMonitorUWP.Code;
+using System.Diagnostics;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +27,7 @@ namespace SystemMonitorUWP.Pages
         public AboutPage()
         {
             this.InitializeComponent();
+            CurrentVersionText.Text = "Current Version: " + Updater.Instance.currentVersion;
         }
     }
 }
